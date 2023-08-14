dd0b8b0ce4fcc3987549c17984ca22d6b20879e9
tlaloc69 <tlaloc1776@gmail.com>
2023-08-13 20:53:13 -0500

    Succesfully ran all restructured pytests

--- test_account.py
+++ test_account.py
@@ -22,62 +22,24 @@ class TestAccount:
         assert self.a1.deposit(-50) is False
         assert self.a1.get_balance() == pytest.approx(0)
 
-        assert self.a1.deposit(50) is True
-        assert self.a1.get_balance() == pytest.approx(50)
+        assert self.a1.deposit(50.50) is True
+        assert self.a1.get_balance() == pytest.approx(50.50)
 
         assert self.a1.deposit(0) is False
-        assert self.a1.deposit() == pytest.approx(0)
+        assert self.a1.get_balance() == pytest.approx(50.50)
 
     def test_withdraw(self):
         assert self.a2.withdraw(-500) is False
-        assert self.a2.withdraw() == pytest.approx(0)
+        assert self.a2.get_balance() == pytest.approx(0)
 
         assert self.a2.withdraw(0) is False
-        assert self.a2.withdraw() == pytest.approx(0)
+        assert self.a2.get_balance() == pytest.approx(0)
 
-        assert self.a2.withdraw(550) is False
-        assert self.a2.withdraw() == pytest.approx(0)
+        assert self.a2.withdraw(5.50) is False
+        assert self.a2.get_balance() == pytest.approx(0)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.a2.deposit(50.00)
+        assert self.a2.withdraw(49.00) is True
+        assert self.a2.get_balance() == pytest.approx(1.00)
+        
 
